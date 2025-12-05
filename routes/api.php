53734299e4288f9c632e7d3742345fd0681ef3b6
Freddy Achu Gaspar <freddycador321@gmail.com>
2025-12-04 20:55:51 -0400

    creacion-modelo-club

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,13 @@
 
 use App\Http\Controllers\ArbitroController;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\ClubController;
+use App\Http\Controllers\EquipoController;
+use App\Http\Controllers\EquipoJugadorController;
+use App\Http\Controllers\JugadorController;
 use App\Http\Controllers\UserController;
+use App\Models\equipo_jugador;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,6 +25,11 @@ use Illuminate\Support\Facades\Route;
 Route::post('/login', [AuthController::class,'authenticate']);
 Route::apiResource('/usuario', UserController::class);
 Route::apiResource('/arbitro', ArbitroController::class);
+Route::apiResource('/jugador', JugadorController::class);
+Route::apiResource('/equipo', EquipoController::class);
+Route::apiResource('/equipo_jugador', EquipoJugadorController::class);
+Route::apiResource('/club', ClubController::class);
+Route::apiResource('/categoria', CategoriaController::class);
 
 Route::middleware('auth:api')->group(function(){
     
